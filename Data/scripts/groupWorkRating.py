b5fc81684ffa6ed92d92804bc17fb86c4c90f688
João Daniel Silva <joao.daniel.silva@tecnico.ulisboa.pt>
2019-10-27 23:08:37 +0000

    group data

---
+++ Data/scripts/groupWorkRating.py
@@ -0,0 +1,31 @@
+import json
+
+f=open("person_work_rating.json")
+s=open("yearOfAppearance.json")
+table = json.load(f)
+appearance_table = json.load(s)
+final = {k:[] for k in range(-10,11)}
+print(final)
+
+for person in table:
+    year_svu = appearance_table[person]
+    for year_working in table[person]:
+        key = int(year_working) - int(year_svu)
+        if key != 0 and -10 <= key <= 10: #works on year of svu dont count
+            freq = table[person][year_working]
+            final[key].append(freq)
+
+avg = {}
+for key in final:
+    if key != 0:
+        print(key)
+        num_works = len(final[key])
+        total = sum(final[key])
+        avg[key] = total / num_works
+
+
+f.close()
+s.close()
+out = open("group_work_rating.json", "w")
+json.dump(avg,out, indent=1)
+out.close()
